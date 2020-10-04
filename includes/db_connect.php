af4c898057c84fa318cba817c272058f2081cf75
brobertson1 <rrober18@lesley.edu>
2020-10-03 21:14:10 -0400

    added db connect file

---
+++ includes/db_connect.php
@@ -0,0 +1,14 @@
+<?php
+
+$dbServer = "localhost";
+$dbUsername = "root";
+$dbPassword = "root";    
+$dbName = "blog_cms";
+
+//creat connection
+
+$conn = mysqli_connect($dbServer,$dbUsername,$dbPassword,$dbName);
+?>
+
+
+
