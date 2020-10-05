c2078631954e8010366c1a78a67ef8f132c96c3a
brobertson1 <rrober18@lesley.edu>
2020-10-04 22:46:13 -0400

    add: submit post file and post submit connect form

--- post-submission.php
+++ post-submission.php
@@ -6,17 +6,17 @@
             <h1>Post Submission Page</h1>
             <p>If you have an idea for a post, we'd love to hear from you. Please fill out this form to submit your idea for a post.</p>
             <h2>Submission Form</h2>
-            <form class="post-form">
+            <form class="post-form" action="submit-post.php" method="POST">
                 <label>Title</label><br>
-                <input type="text"> <br><br>
+                <input type="text" name="title"> <br><br>
                 <label>Author</label><br>
-                <input type="text"> <br><br>
+                <input type="text" name="author"> <br><br>
                 <label>Today's Date</label><br>
-                <input type="date"> <br><br>
+                <input type="date" name="date"> <br><br>
                 <label>Post</label><br>
-                <textarea rows="7" cols="40">
+                <textarea rows="7" cols="40" name="content">
                 </textarea><br><br>
-                <input type="submit">
+                <input type="submit" name ="submit">
             </form>
             </div>
         </main>
